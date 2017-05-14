46fffa17d61bdd6d36c4969a3af9ed8049544715
eabohorquezg <eabohorquezg@unal.edu.co>
2017-05-13 21:07:41 -0500

    Transitions with time

--- src/Gui.java
+++ src/Gui.java
@@ -1,30 +1,50 @@
+
 import processing.core.PApplet;
 
 import java.util.ArrayList;
-import java.util.StringJoiner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 /**
  * Created by kevincastro on 5/12/17.
  */
 public class Gui extends PApplet{
 
+    private static Petrinet pn = new Petrinet("PetriNet");
+    
     public static void main(String[] args) {
+        
+        PApplet.main("Gui");
+        
+        Transition t1 = pn.transition("t1",2);
+        Transition t2 = pn.transition("t2",1.5);
 
-        PApplet.main("Gui", args);
+        Place p1 = pn.place("p1", 1);
+        Place p2 = pn.place("p2");
+
+        Arc a1 = pn.arc("a1", p1, t1);
+        Arc a2 = pn.arc("a2", t1, p2);
+        Arc a3 = pn.arc("a3", p2, t2);
+        Arc a4 = pn.arc("a4", t2, p1);
+
+        //test
+        try {
+            System.out.println("Examples of fires");                
+            t1.setDelayTime(t1.getRate());
+            System.out.println("Transition 1 with delay time: "+t1.getDelayTime());
+            Thread.sleep((long)t1.getDelayTime()*1000);            
+            System.out.println("Fire 1");
+            t2.setDelayTime(t2.getRate());
+            System.out.println("Transition 2 with delay time: "+t2.getDelayTime());
+            Thread.sleep((long) t2.getDelayTime()*1000);
+            System.out.println("Fire 2");
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Gui.class.getName()).log(Level.SEVERE, null, ex);
+        }                
+                
     }
-
-    Petrinet pn = new Petrinet("PetriNet");
-    Transition t1 = pn.transition("t1");
-    Transition t2 = pn.transition("t2");
-
-    Place p1 = pn.place("p1", 1);
-    Place p2 = pn.place("p2");
-
-    Arc a1 = pn.arc("a1", p1, t1);
-    Arc a2 = pn.arc("a2", t1, p2);
-    Arc a3 = pn.arc("a3", p2, t2);
-    Arc a4 = pn.arc("a4", t2, p1);
-
+    
     public void settings(){
         size(500,500);
 
@@ -73,4 +93,5 @@ public class Gui extends PApplet{
 
         }
     }
+    
 }
